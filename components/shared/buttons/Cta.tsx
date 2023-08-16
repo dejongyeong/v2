44ad0cf1dc38607c1656110307d91a17bf93821e
De Jong Yeong <dejong.re2@gmail.com>
2023-08-16 23:02:37 +0100

    refactor css on cta

--- components/shared/buttons/Cta.tsx
+++ components/shared/buttons/Cta.tsx
@@ -2,17 +2,21 @@ import Link from "next/link";
 
 const Cta = () => {
   return (
-    <div className="mt-6 flex flex-row gap-4">
-      <Link href="/#contact" passHref>
-        <a className="inline-block px-5 py-2 mx-auto text-white bg-blue-600 rounded-full hover:bg-blue-700 hover:drop-shadow-lg md:mx-0">
-          Contact Me
-        </a>
-      </Link>
-      <Link href="/#portfolio" passHref>
-        <a className="inline-block  px-5 py-2 mx-auto text-blue-600 rounded-full hover:bg-blue-600 hover:text-white hover:drop-shadow-lg md:mx-0">
-          Portfolio
-        </a>
-      </Link>
+    <div className="mt-6 flex flex-col sm:flex-row gap-4">
+      <div>
+        <Link href="/#contact" passHref>
+          <a className="inline-block px-5 py-2 mx-auto text-white bg-blue-600 rounded-full border border-blue-600 hover:bg-blue-700 hover:drop-shadow-lg md:mx-0 whitespace-nowrap">
+            Contact Me
+          </a>
+        </Link>
+      </div>
+      <div>
+        <Link href="/#portfolio" passHref>
+          <a className="inline-block px-5 py-2 mx-auto text-blue-600 rounded-full border border-blue-600 hover:bg-blue-600 hover:text-white hover:drop-shadow-lg md:mx-0">
+            Portfolio
+          </a>
+        </Link>
+      </div>
     </div>
   );
 };
